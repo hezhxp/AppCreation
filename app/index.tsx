403b01f271a9830d6dcb314f83013d586724496c
hezhxp <hcalub87@gmail.com>
2026-08-27 23:32:21 +0100

    Made linking button for onboarding

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
 import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-app-bg">
+      <Text className="text-xl font-bold text-text-primary">
         testing GitHub Repo Transfer Yay!
       </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-neon-lime py-3.5 text-text-primary">
+        Go to onboarding
+      </Link>
     </View>
   );
 }
